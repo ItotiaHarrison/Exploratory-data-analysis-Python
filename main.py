6db7a673429f62d792ac0e8f9c6be94c62f7f052
Harrison Itotia <harrisonitotia@gmail.com>
2023-03-03 10:59:34 +0300

    drop missing values

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
-# import pandas as pd
+import pandas as pd
 import numpy as np
-
-import csv
-import matplotlib as plt
-from matplotlib import rcParams
 import seaborn as sns
 
-plt.style.use("ggplot")
-rcParams['figure.figsize'] = (12,6)
+import matplotlib.pyplot as plt
 
-# df = pd.read_csv(r'F:/Python assignment/assets/Survey2020.csv')
-# print(df)
 
-with open("F:\Python assignment\assets\Survey2020.csv") as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        print(row)
+
+df = pd.read_csv('/home/raphael/Desktop/Lux/Exploratory-data-analysis-Python/assets/T Salary Survey EU 2019.csv')
+# display the top 5 rows
+print(df.head(5))
+
+# display the bottom 5 rows
+print(df.tail(5))
+
+# number of rows and columns in the data
+print(df.shape) 
+# The data comprises of 991 observations and 23 characteristics
+
+
+
+# display various summary statistics
+print(df.describe())
+# The mean value is less than the median value(50%) in the number of vacation days column 
+# There is a large difference between the 75%tile and the max values of predictors "number 0f vacation days", "number of home office days per month", "yearly brutto salary".
+# The above observations suggest that there are extreme values-outliers in the dataset.
+
+
+
+# columns and corresponding data types
+print(df.info())
+# Data has float, integer and object values
+
+
+
+# Finding null values in the dataset
+print(df.isnull().sum())
+
+
+# Dropping the null values
+df = df.dropna() 
+print(df.count())
+
+
+
+# Checking for rows with duplicate data
+duplicate_rows_df = df[df.duplicated()]
+print(duplicate_rows_df.shape)
+
+
